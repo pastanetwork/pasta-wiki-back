52948e4de1d8463a2a9a52a9a7145a4626e01b5f
St4lV <152074070+St4lV@users.noreply.github.com>
2025-10-19 17:34:10 +0200

    clickable header title

--- client/public/global.js
+++ client/public/global.js
@@ -1,4 +1,4 @@
 (()=>{
     const header = document.querySelector("header")
-    header.innerHTML="<img id='pasta-logo-header' src='/public/pasta_logo.svg' alt='Logo Pastanetwork'><h1>Pastanetwork wiki</h1>"
+    header.innerHTML="<a href='/' class='flex-al-it-center'><img id='pasta-logo-header' src='/public/pasta_logo.svg' alt='Logo Pastanetwork'><h1>Pastanetwork wiki editor</h1></a>"
 })();
