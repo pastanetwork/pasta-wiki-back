43636f45fc24487ee2055d16cb68163d14bc2fff
St4lV <152074070+St4lV@users.noreply.github.com>
2025-11-10 17:32:44 +0100

    refresh and searchbar on articles and categories dashboard pages

--- client/dashboard/categories/categories.js
+++ client/dashboard/categories/categories.js
@@ -3,6 +3,9 @@ import { fetchData, updateLang } from "/public/global.js"
 const category_list_holder = document.getElementById("categories-list-holder");
 const new_category_lang_selector = document.getElementById("categories-upper-comp-toolbar-new-category-lang");
 
+const refresh_btn = document.getElementById("categories-list-refresh");
+const search_input = document.getElementById("categories-list-search-input");
+
 const icons = {
     trash: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg>`,
     pen: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/></svg>`
@@ -10,7 +13,7 @@ const icons = {
 
 let langs_list = "";
 let categories_data = "";
-async function refreshData() {
+async function refreshData(){
     await refreshLangs();
     await refreshCategories();
     updateLang();
@@ -213,4 +216,28 @@ function setAlertOpened(val,dom){
         alert_div.dataset.opened=false
         alert_div.innerHTML=""
     }
+}
+
+refresh_btn.addEventListener("click",async function(){
+    await refreshData();
+});
+
+search_input.addEventListener("input",function(){
+    const elements_list = document.querySelectorAll(".category-data-holder");
+    const searched_value = search_input.value;
+    for (let el of elements_list){
+        if (searched_value===""){
+            el.style.display="table-row"
+        } else {
+            if (minimise(el._categoryData.title).includes(minimise(searched_value))){
+                el.style.display="table-row"
+            } else {
+                el.style.display="none"
+            }
+        }
+    }
+});
+
+function minimise(input){
+    return input.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
