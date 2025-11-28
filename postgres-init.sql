a7b9a5e711483fd2d4d0e80628b5bdf867fefef1
St4lV <152074070+St4lV@users.noreply.github.com>
2025-11-28 03:07:52 +0100

    Fixed permissions on db init script

--- postgres-init.sql
+++ postgres-init.sql
@@ -143,7 +143,9 @@ INSERT INTO perms.roles_perms (role_id, perm_id) VALUES
     (2, 4),
     (2, 5),
     (2, 6),
-    (2, 7);
+    (2, 7),
+    (2, 8),
+    (2, 9);
 
 INSERT INTO perms.roles_perms (role_id, perm_id) VALUES 
     (3, 1),
